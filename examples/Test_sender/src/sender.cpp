a86628a3885020aa538aa24fb7db22284b9d8f3f
haberturdeur <tomas.rohlinek@outlook.cz>
2020-02-09 18:55:43 +0100

    chnged some features

---
+++ examples/Test_sender/src/sender.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include "Janus_decoder.h"
+#include <iostream>
+
+Decoder decoder;
+
+Janus_decoder_settings_t settings;
+void setup() {
+  settings.u_port = UART_NUM_2;
+  decoder.init(settings);
+}
+
+std::vector<uint8_t> data= {0x14, 0x15, 0x16, 0x17, 0x18, 0x20};
+using namespace std;
+void loop() {
+  message_t out;
+  // put your main code here, to run repeatedly:
+  decoder.send(0x11, 0x12, 0x13, data);
+  // out = decoder.receive();
+
+
+  // delay(100);
+}
