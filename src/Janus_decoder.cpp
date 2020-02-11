52bdef0f5069979e75b5b4bd71a17ee8aa5d9320
haberturdeur <tomas.rohlinek@outlook.cz>
2020-02-11 22:25:16 +0100

    changed checksum calculation - still not working

--- src/Janus_decoder.cpp
+++ src/Janus_decoder.cpp
@@ -35,25 +35,25 @@ void Decoder::init(Janus_decoder_settings_t settings){
     uart_set_mode(uart_port, UART_MODE_RS485_HALF_DUPLEX);
 }
 
-void Decoder::send(uint8_t rec_addr,uint8_t sen_addr, uint8_t cmd, std::vector<uint8_t>* src){
-    uint8_t length_of_data = src->size();
-    uint8_t control = rec_addr + sen_addr + cmd;
-    for(int i = 0; i<length_of_data; i++){
-        control += (*src)[i];
-    }
-    control = control % 256;
-        #if debug_decoder
-            printf("Check: %u\n", control);
-        #endif
+void Decoder::send(uint8_t rec_addr,uint8_t sen_addr, uint8_t cmd, std::vector<uint8_t>& src){
+    uint8_t length_of_data = src.size();
+    message_t out;
+    out.cmd = cmd;
+    out.data = src;
+    out.length = length_of_data;
+    out.rec_addr = rec_addr;
+    out.send_addr = sen_addr;
+
+    out.check = calculateChecksum(out);
     uart_write_bytes(uart_port, &SOH, 1);
     uart_write_bytes(uart_port, (const char*)&rec_addr, 1);
     uart_write_bytes(uart_port, (const char*)&sen_addr, 1);
     uart_write_bytes(uart_port, (const char*)&cmd, 1);
     uart_write_bytes(uart_port, (const char*)&length_of_data, 1);
-    uart_write_bytes(uart_port, (const char*)&control, 1);
+    uart_write_bytes(uart_port, (const char*)&out.check, 1);
     char out_data = 0;
     for(int i = 0; i<length_of_data; i++){
-        out_data = (*src)[i];
+        out_data = src[i];
         uart_write_bytes(uart_port, (const char*)(&out_data), 1);
     }
     vTaskDelay(200 / portTICK_PERIOD_MS);
@@ -63,27 +63,80 @@ void Decoder::send(uint8_t rec_addr,uint8_t sen_addr, uint8_t cmd, std::vector<u
 message_t Decoder::receive(){
     uint8_t read_byte;
     received_message.data.clear();
+        #if debug_decoder
+            printf("Receiving message");
+            #endif
     static uint8_t* rec_buff = (uint8_t*) malloc(buffer_size);
     do{
         uart_read_bytes(uart_port, &read_byte, 1, PACKET_READ_TICS);
+        #if debug_decoder
+            printf(".");
+            #endif
     }while (read_byte != SOH);
+        #if debug_decoder
+            printf("\n Parsing message.\n");
+            #endif
     uart_read_bytes(uart_port, rec_buff, 1, PACKET_READ_TICS);
     received_message.rec_addr = rec_buff[0];
+        #if debug_decoder
+            printf("Recepient: %u\n", received_message.rec_addr);
+            #endif
     uart_read_bytes(uart_port, rec_buff, 1, PACKET_READ_TICS);
     received_message.send_addr = rec_buff[0];
+        #if debug_decoder
+            printf("Sender: %u\n", received_message.send_addr);
+            #endif
     uart_read_bytes(uart_port, rec_buff, 1, PACKET_READ_TICS);
     received_message.cmd = rec_buff[0];
+        #if debug_decoder
+            printf("Command: %u\n", received_message.cmd);
+            printf((const char*)&received_message.cmd);
+            #endif
     uart_read_bytes(uart_port, rec_buff, 1, PACKET_READ_TICS);
     received_message.length = rec_buff[0];
+        #if debug_decoder
+            printf("Length: %u\n", received_message.length);
+            #endif
     uart_read_bytes(uart_port, rec_buff, 1, PACKET_READ_TICS);
     received_message.check = rec_buff[0];
+    #if debug_decoder
+            printf("Check: %u\n", received_message.check);
+            printf("Data: ");
+            #endif
     for(int i = 0; i<received_message.length; i++){
         uart_read_bytes(uart_port, rec_buff, 1, PACKET_READ_TICS);
         received_message.data.push_back(rec_buff[0]);
+        #if debug_decoder
+            printf("%u", received_message.data[i]);
+            #endif
     } 
     // received_message = parse_message(rec_buff);
     uart_flush(uart_port);
+
+    
+    if(calculateChecksum(received_message)==received_message.check) 
+        received_message.correct = 1; 
+        else 
+        received_message.correct = 0;
+        #if debug_decoder
+            printf("Check_calc: %u\n", calculateChecksum(received_message));
+            printf("Check_sum check: %u\n", received_message.correct);
+        #endif
+
     return received_message;
+}
+
+uint8_t Decoder::calculateChecksum(message_t &in){
+    uint32_t check_calc = 0;
+
+    check_calc += in.rec_addr;
+    check_calc += in.send_addr;
+    check_calc += in.cmd;
+    check_calc += in.length;
+    for( auto &i : in.data)
+        check_calc += in.data[i];
+    uint8_t out = check_calc % 256;
+    return out;
 }
 
 
