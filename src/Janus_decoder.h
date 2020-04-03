e9351ddab2b3ac5a21f1258343ff3f226780834d
haberturdeur <tomas.rohlinek@outlook.cz>
2020-04-03 18:12:42 +0200

    Split header file

--- src/Janus_decoder.h
+++ src/Janus_decoder.h
@@ -4,54 +4,7 @@
 
 #include "driver/uart.h"
 #include <vector>
-
-static uart_config_t defaultUartConfig = {
-    .baud_rate = BAUD_RATE,
-    .data_bits = UART_DATA_8_BITS,
-    .parity = UART_PARITY_DISABLE,
-    .stop_bits = UART_STOP_BITS_1,
-    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-    .rx_flow_ctrl_thresh = 122,
-};
-
-enum byte_order {
-    START_BYTE,
-    RECEPIENT_ADDR_BYTE,
-    SENDER_ADDR_BYTE,
-    COMMAND_BYTE,
-    LENGTH_OF_DATA_BYTE,
-    CHECK_BYTE,
-    FIRST_DATA_BYTE
-};
-
-enum message_order {
-    RECEPIENT_ADDR,
-    SENDER_ADDR,
-    COMMAND,
-    LENGHT_OF_DATA,
-    CHECK,
-    FIRST_DATA
-};
-
-struct message_t {
-    uint8_t recepientAddress;
-    uint8_t senderAddress;
-    uint8_t cmd;
-    uint8_t length;
-    uint8_t check;
-    std::vector<uint8_t> data;
-    bool correct { 0 };
-};
-
-struct Janus_decoder_settings_t {
-    uart_port_t uartPort = UART_NUM_2;
-    int rxPin = DEFAULT_PIN_RXD;
-    int txPin = DEFAULT_PIN_TXD;
-    int rtsPin = DEFAULT_PIN_RTS;
-    int ctsPin = DEFAULT_PIN_CTS;
-    uart_config_t* uartConfigPtr = &defaultUartConfig;
-    int uartBufferSize = BUF_SIZE;
-};
+#include "Janus_decoder_types.h"
 
 static const Janus_decoder_settings_t JanusDecoderDefaultSettings;
 
